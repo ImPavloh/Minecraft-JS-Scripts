0e4c53b73beca8691e32ea9b4b1b4d3c84efbcc1
Pavloh <89773173+ImPavloh@users.noreply.github.com>
2022-09-26 16:49:25 +0200

    Update player_time_limit.js

--- player_time_limit.js
+++ player_time_limit.js
@@ -1,9 +1,9 @@
-var timeLimit = 14400;
+var timeLimit = 14400; //Tiempo en segundos Ejemplo: 4 horas = 14400
 
 onEvent("player.tick", event => {
     let player = event.player;
     
-    if ((event.level.time % 20) === 0) {
+    if ((event.level.time % 20) === 0) { //Si es nuevo o es un día distinto comienza el tiempo desde 0 y suma 1 cada segundo
             let today = new Date().toDateString();
             let playername = player.name.string;
             let data = event.server.persistentData;
@@ -14,9 +14,9 @@ onEvent("player.tick", event => {
                 playerData.timePlayed = 0;
                 playerData.lastPlayed = today;
             }
-            playerData.timePlayed += 1;
+            playerData.timePlayed += 1; 
 
-            if (playerData.timePlayed >= timeLimit) {
+            if (playerData.timePlayed >= timeLimit) { //Expulsa al jugador (sin OP) una vez superado el tiempo límite
                 console.log("El jugador" + playername + " ha superado el tiempo límite.");
                 console.info("El jugador" + playername + " ha superado el tiempo límite.");
                 if (!event.player.op) {
@@ -28,7 +28,7 @@ onEvent("player.tick", event => {
 });
 
 
-function onRtpExecute(ctx) {
+function onRtpExecute(ctx) { //calcula tiempo y lo dice al jugador y a la consola
 
     const player = ctx.source.getPlayerOrException().asKJS();
     let playerData = player.server.persistentData[player.name.string];
@@ -42,7 +42,7 @@ function onRtpExecute(ctx) {
 }
 
 
-function commandRegistry(ev) {
+function commandRegistry(ev) { //crea un comando
     const {
         commands: Commands
     } = ev;
